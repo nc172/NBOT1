2b925df0e5a6d90897f13d8d5fe8fb76f1c1bb1f
nc172 <117373842+nc172@users.noreply.github.com>
2023-03-27 21:40:37 -0400

    Add files via upload

---
+++ ProjectNBot1/src/main/java/org/example/BotListeners.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.events.channel.ChannelDeleteEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class BotListeners extends ListenerAdapter {
+
+    /**
+     * get message, if message start with hi then say the greeting
+     * @param: event (message) from text channel
+     */
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        //get author name
+        String authorName = event.getAuthor().getName();
+        if (event.getAuthor().isBot()) {
+            // Ignore messages from other bots
+            return;
+        }
+
+        // Get the message content
+        String messageContent = event.getMessage().getContentRaw();
+
+        // Check if the message starts with a specific prefix
+        if (messageContent.startsWith("hi")) {
+            System.out.println("wow");
+            // Send a response message to the same channel
+            event.getChannel().sendMessage("Hello, " + authorName + "!").queue();
+        }
+    }
+
+
+    /**
+     * announce a channel elimination on desired channel (main channel)
+     * @param: event(delete action) from any channel
+     */
+    @Override
+    public void onChannelDelete(ChannelDeleteEvent event) {
+        String channelName = event.getChannel().getName();
+        TextChannel general = event.getGuild().getTextChannelById(895170980970901507L);//get id from general channel
+
+        if(general != null){
+            general.sendMessage("the channel \"" + channelName + "\" was deleted").queue();
+        }
+    }
+
+}
